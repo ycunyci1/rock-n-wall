35229c0de469db2231279ea129dd58bc30d9c4eb
chequite1 <ycunyci@mail.ru>
2024-04-06 13:50:37 +0300

    add: redirect to admin from homepage

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::get('/', function () {
+    return redirect('/admin');
+});
 
