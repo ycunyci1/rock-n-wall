989acc4144b83016129342bd8f935ad9c9a484aa
chequite1 <ycunyci@mail.ru>
2024-10-29 03:34:57 +0300

    send database backups only for prod

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('backup:send')->everyFifteenMinutes();
+        if (config('app.env') === 'production') {
+            $schedule->command('backup:send')->everyFifteenMinutes();
+        }
         // $schedule->command('inspire')->hourly();
     }
 
